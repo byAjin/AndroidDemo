df73099212d6b6a8db9019818f5720c8a7c1b3df
spean90 <spean90@gmail.com>
2015-02-27 14:33:26 +0800

    datepicker dialog  timepicker dialog

--- src/com/spean90/androiddemo/DateTimePickerActivity.java
+++ src/com/spean90/androiddemo/DateTimePickerActivity.java
@@ -3,7 +3,14 @@ package com.spean90.androiddemo;
 import java.util.Calendar;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.TimePicker;
@@ -16,10 +23,12 @@ import android.widget.TimePicker.OnTimeChangedListener;
 * @date 2015-2-27 
 *
  */
-public class DateTimePickerActivity extends Activity {
+public class DateTimePickerActivity extends Activity implements OnClickListener {
 	
 	private DatePicker datePicker;
 	private TimePicker timePicker;
+	private Button dateBtn,timeBtn;
+	Calendar calendar = Calendar.getInstance();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,7 @@ public class DateTimePickerActivity extends Activity {
 		setContentView(R.layout.datetimeactivity);
 		datePicker = (DatePicker) findViewById(R.id.datePicker);
 		timePicker = (TimePicker) findViewById(R.id.timePicker);
-		Calendar calendar = Calendar.getInstance();
+		
 		datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1,calendar.get(Calendar.DAY_OF_MONTH) , new OnDateChangedListener() {
 			@Override
 			public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -41,6 +50,38 @@ public class DateTimePickerActivity extends Activity {
 				setTitle(hourOfDay+":"+minute);
 			}
 		});
+		
+		dateBtn = (Button) findViewById(R.id.datepicker_dialog);
+		timeBtn = (Button) findViewById(R.id.timepicker_dialog);
+		dateBtn.setOnClickListener(this);
+		timeBtn.setOnClickListener(this);
+	}
+	
+	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.datepicker_dialog:
+			new DatePickerDialog(this, new OnDateSetListener() {
+				@Override
+				public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+					setTitle(year+"-"+(monthOfYear+1)+"-"+dayOfMonth);
+				}
+			}, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+			break;
+		case R.id.timepicker_dialog:
+			new TimePickerDialog(this, new OnTimeSetListener() {
+				@Override
+				public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+					setTitle(hourOfDay+":"+minute);
+				}
+			}, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+			break;
+		default:
+			break;
+		}
+		
 	}
 
 }
